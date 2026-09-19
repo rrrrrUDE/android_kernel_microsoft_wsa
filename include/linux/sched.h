c5ee6bec1500cf6262da084e0558f8c2e1a7d1d1
rrrrrUDE! <xkandemo666@gmail.com>
2026-09-19 16:13:49 +0800

    My first change

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1078,10 +1078,8 @@ struct task_struct {
 	struct nameidata		*nameidata;
 
 #ifdef CONFIG_SYSVIPC
-	// struct sysv_sem			sysvsem;
-	/* sysvsem is in the ANDROID_KABI_RESERVE(1) field below */
+    // struct sysv_sem			sysvsem;
 	// struct sysv_shm			sysvshm;
-	/* sysvshm is in the ANDROID_KABI_RESERVE(1) field below */
 #endif
 #ifdef CONFIG_DETECT_HUNG_TASK
 	unsigned long			last_switch_count;
@@ -1520,12 +1518,9 @@ struct task_struct {
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
 
-#if defined(CONFIG_SYSVIPC)
-	// struct sysv_sem			sysvsem;
+#ifdef CONFIG_SYSVIPC
 	ANDROID_KABI_USE(6, struct sysv_sem sysvsem);
-	// struct sysv_shm			sysvshm;
-	_ANDROID_KABI_REPLACE(ANDROID_KABI_RESERVE(7); ANDROID_KABI_RESERVE(8),
-						  struct sysv_shm sysvshm);
+	_ANDROID_KABI_REPLACE(ANDROID_KABI_RESERVE(7); ANDROID_KABI_RESERVE(8), struct sysv_shm sysvshm);
 #else
 	ANDROID_KABI_RESERVE(6);
 	ANDROID_KABI_RESERVE(7);
